8bd4fac06fcb6ffefbe951c5893c2813283235cb
musanurbadr <Musanur6685@gmail.com>
2025-12-29 18:04:44 +0300

    How To: Add an Admin Role

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,6 @@
-class User < ApplicationRecord
-  rolify
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable 
+         :recoverable, :rememberable, :validatable, :trackable, :lockable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # 1. Ana Sayfa (Site açılınca görünen yer)
   root "dashboard#index"
   
